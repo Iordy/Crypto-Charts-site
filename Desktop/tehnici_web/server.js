bb7e769b53295de25a450c901d72ff3b0c245f66
Iordy <84620187+Iordy@users.noreply.github.com>
2023-05-09 20:53:26 +0300

    Update server.js
    Resolved users json file issue

--- Desktop/tehnici_web/server.js
+++ Desktop/tehnici_web/server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 const bcrypt = require('bcrypt');
 
-const users  = [];
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -46,27 +45,22 @@ app.get('/register', (req, res) => {
 
 }); 
 
+let users = [];
 
 app.post('/register', async (req, res, next) => {
+
 
     const user = {
         id: Date.now().toString(),
         username: req.body.username,
         email: req.body.email
     }
-    
-    users.push(user);
 
-    fs.writeFile('users.json', JSON.stringify(user), {flag: "a"}, (err) => {
+   const temp = addUser(user);
 
-        if (err) {
-            console.log(err);
-        }
-
-
-    });
-  
+  if(temp)
     res.redirect('/login');
+    else res.redirect('/register', {message: 'User already exists'})
 
   });
 
@@ -76,9 +70,8 @@ app.get('/login', async (req, res) => {
     res.render('login');
 
     const user = users.find(user => user.username === req.body.username);
-    if (user == null) {
-        return res.status(400).send('Cannot find user');
-    }
+    
+
 
     try {
 
@@ -103,6 +96,53 @@ app.get('/login', async (req, res) => {
 
 
 
+
+function getUsers(){
+    return new Promise((resolve, reject) =>{
+        
+        const filePath = path.join(cwd(), 'users.json');
+
+        fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
+            if(err) {
+                resolve([]);
+            }
+            try {
+                resolve(JSON.parse(data));
+            } 
+            catch(err) {
+                resolve([]);
+            }
+        })
+
+    })
+}
+
+
+async function addUser(user){
+
+    const users = await getUsers();
+
+    // if the user is not in the array we add it
+    if(containsObject(user, users) == false){
+        users.push(user);
+        fs.writeFile('users.json', JSON.stringify(users), (after) => {});
+        return true;
+    }
+
+    return false;
+
+}
+
+
+function containsObject(obj, list) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i]['email'] == obj['email']) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 
 
