a142cd3285872af2d8b59c9a8b4096b62a2ac432
Iordy <andrei.iordache2017@gmail.com>
2023-05-09 19:27:53 +0300

    made users file and set up login

--- Desktop/tehnici_web/server.js
+++ Desktop/tehnici_web/server.js
@@ -1,7 +1,21 @@
 
 const express = require('express');
 
+const {cwd} = require('process');
+
+const path = require('path');
+
+const fs = require('fs');
+
 const app = express();
+
+app.use(express.json());
+
+const bcrypt = require('bcrypt');
+
+const users  = [];
+
+app.use(express.urlencoded({ extended: false }));
 
 
 app.set('view engine', 'ejs');
@@ -24,13 +38,70 @@ app.get('/watchlist', (req, res) => {
 );
 
 
+app.get('/register', (req, res) => {
+
+    res.render('register');
+
+    console.log(users);
+
+}); 
 
 
-const SignIn = require('./signIn');
-const SignUp = require('./register');
+app.post('/register', async (req, res, next) => {
 
-app.use('/SignIn', SignIn);
-app.use('/register', SignUp)
+    const user = {
+        id: Date.now().toString(),
+        username: req.body.username,
+        email: req.body.email
+    }
+    
+    users.push(user);
+
+    fs.writeFile('users.json', JSON.stringify(user), {flag: "a"}, (err) => {
+
+        if (err) {
+            console.log(err);
+        }
+
+
+    });
+  
+    res.redirect('/login');
+
+  });
+
+
+app.get('/login', async (req, res) => {
+
+    res.render('login');
+
+    const user = users.find(user => user.username === req.body.username);
+    if (user == null) {
+        return res.status(400).send('Cannot find user');
+    }
+
+    try {
+
+        if (await bcrypt.compare(req.body.password, user.password)) {
+
+            res.send('Success');
+
+        } else {
+
+            res.send('Not Allowed');
+
+        }
+
+    }
+    catch {
+
+        res.status(500).send();
+
+    }
+
+});
+
+
 
 
 
